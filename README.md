4247cb29b1d1fa26637788604e3b3e54544dfcda
Adrien Pyke <adpyke@gmail.com>
2019-09-20 13:36:20 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Sql Formatter
+
+Format SQL files using the [sql-formatter](https://github.com/zeroturnaround/sql-formatter) npm package.
+
+## Configuration
+
+**`sql-formatter.dialect`**: Changes which dialect to format with (`sql`: Standard SQL, `n1ql`: Couchbase N1QL, `db2`: IBM DB2, `pl/sql`: Oracle PL/SQL). Defaults to `sql`.
---
+++ src/extension.js
@@ -0,0 +1,30 @@
+const vscode = require('vscode');
+const sqlFormatter = require('sql-formatter');
+
+const getRange = document =>
+	new vscode.Range(
+		0,
+		0,
+		document.lineCount - 1,
+		document.lineAt(document.lineCount - 1).range.end.character
+	);
+
+const getConfig = () => {
+	const editorSettings = vscode.workspace.getConfiguration('editor', null);
+	const extensionSettings = vscode.workspace.getConfiguration('sql-formatter', null);
+	const indent = editorSettings.get('insertSpaces')
+		? ' '.repeat(editorSettings.get('tabSize'))
+		: '\t';
+	const language = extensionSettings.get('dialect', 'sql');
+	return { indent, language };
+};
+
+const format = text => sqlFormatter.format(text, getConfig());
+
+module.exports.activate = () => {
+	vscode.languages.registerDocumentFormattingEditProvider('sql', {
+		provideDocumentFormattingEdits: document => [
+			vscode.TextEdit.replace(getRange(document), format(document.getText()))
+		]
+	});
+};
