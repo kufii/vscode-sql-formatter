d096d0a24e4aa1d9f212b46ba5206502a1c34c30
Adrien Pyke <adpyke@gmail.com>
2019-10-15 17:56:47 -0400

    fix warning

--- src/extension.js
+++ src/extension.js
@@ -8,7 +8,8 @@ const getSetting = (group, key, def) => {
 	const settings = vscode.workspace.getConfiguration(group, null);
 	const editor = vscode.window.activeTextEditor;
 	const language = editor && editor.document && editor.document.languageId;
-	const languageSettings = language && vscode.workspace.getConfiguration().get(`[${language}]`);
+	const languageSettings =
+		language && vscode.workspace.getConfiguration(null, null).get(`[${language}]`);
 	let value = languageSettings && languageSettings[`${group}.${key}`];
 	if (value == null) value = settings.get(key, def);
 	return value == null ? def : value;
